738c1a5d921bb9b3577447c476922e5f65cbb68b
DESKTOP-GHEQTT2\Eduardo <ebm7pc@gmail.com>
2017-11-04 18:01:19 -0500

    inician pruebas de rest service

--- src/co/udea/iw/rest/ServicioCliente.java
+++ src/co/udea/iw/rest/ServicioCliente.java
@@ -2,19 +2,24 @@ package co.udea.iw.rest;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import co.udea.iw.dto.Cliente;
-import co.udea.iw.dto.ClienteWS;
 import co.udea.iw.exception.IWDaoException;
+import co.udea.iw.exception.IWServiceException;
 import co.udea.iw.service.ClienteService;
 
 @Path("Cliente")
@@ -25,34 +30,77 @@ public class ServicioCliente {
 	private ClienteService clienteservice;
 	
 	
+	@Path("ListaClientes")
 	@Produces(MediaType.APPLICATION_XML)
 	@GET
-	public List<ClienteWS> obtener() throws RemoteException{ //List<ClienteWS>
-		
-		List<ClienteWS> clientes= new ArrayList<ClienteWS>();
+	public List<Cliente> obtener() throws RemoteException{ //List<ClienteWS>
 		
 		List<Cliente> listaClientes= null;
 		try {
 			
-			listaClientes=clienteservice.obtener(); 
-			
-			for(Cliente cliente : listaClientes) {
-				
-				ClienteWS clienteWS= new ClienteWS();
-				
-				clienteWS.setCedula(cliente.getIdentificacion());
-				clienteWS.setApellidos(cliente.getApellido());
-				clienteWS.setNombres(cliente.getNombre());
-				
-				clientes.add(clienteWS);
-				System.out.println("agrego el cliente ");
-			}
+			listaClientes=clienteservice.obtener();
 			
 		} catch (IWDaoException e) {
 			throw new RemoteException(e.getMessage());
 		}
 		
-		return clientes;
+		return listaClientes;
 	}
-
+	
+	
+	@Path("ObtenerCliente")
+	@Produces(MediaType.APPLICATION_XML)
+	@GET
+	public Cliente obtenerCliente(@QueryParam("ficho") Integer ficho) throws RemoteException, IWServiceException{ //List<ClienteWS>
+		
+		Cliente cliente= null;
+		try {
+			
+			cliente=clienteservice.obtener(ficho);
+			
+		} catch (IWDaoException e) {
+			throw new RemoteException(e.getMessage());
+		} catch (IWServiceException ex) {
+			throw new IWServiceException(ex.getMessage());
+		}
+		
+		return cliente;
+	}
+	
+	
+	@Path("InsertarCliente")
+	@Produces(MediaType.TEXT_PLAIN)
+	@POST
+	public String insertaCliente(@QueryParam("ficho") Integer ficho, @QueryParam("tipo") String tipo,
+			@QueryParam("nombre") String nombre, @QueryParam("apellido") String apellido,
+			@QueryParam("identificacion") Long identificacion, @QueryParam("apto") String apto,
+			@QueryParam("tel") Long tel, @QueryParam("cel") Long cel, @QueryParam("mail") String mail,
+			@QueryParam("vehi") Boolean vehi, @QueryParam("resp") String resp, @QueryParam("fsalida") Date fsalida) 
+					throws RemoteException, IWServiceException{ 
+		try {
+			clienteservice.guardarCliente(ficho, tipo, nombre, apellido, identificacion, apto, tel, cel, mail, vehi, resp, fsalida);
+		} catch (IWDaoException e) {
+			throw new RemoteException(e.getMessage());
+		} catch (IWServiceException ex) {
+			throw new IWServiceException(ex.getMessage());
+		}
+		return "";
+	}
+	
+	@Path("ActualizarCliente")
+	@Produces(MediaType.TEXT_PLAIN)
+	@PUT
+	public String actualizarCliente(@QueryParam("ficho") Integer ficho, @QueryParam("apto") String apto,
+			@QueryParam("tel") Long tel, @QueryParam("cel") Long cel, @QueryParam("mail") String mail,
+			@QueryParam("vehi") Boolean vehi, @QueryParam("fsalida") Date fsalida) 
+					throws RemoteException, IWServiceException{ 
+		try {
+			clienteservice.modificarCliente(ficho, apto, tel, cel, mail, vehi, fsalida);
+		} catch (IWDaoException e) {
+			throw new RemoteException(e.getMessage());
+		} catch (IWServiceException ex) {
+			throw new IWServiceException(ex.getMessage());
+		}
+		return "";
+	}
 }
